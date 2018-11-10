9600f68c37935398afab949439ba893b17b6856d
Janderson <janderson@super.cash>
2018-11-10 11:15:27 -0300

    port bugfix

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ io.on('connection', socket => {
     socket.on("move", moveData => playerMoved(socket, moveData));
 });
 
-const PORT = process.env.PORT || 
+const PORT = process.env.PORT || 3000
 
-server.listen(3000, () => console.log("Server running on port 3000"));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
